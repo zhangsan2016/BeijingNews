b149e9ea191d4e834320d24a897ca9928b6d764e
liu <602694077@qq.com>
2017-06-14 10:08:25 +0800

    2017年6月14日 10:08:20

--- app/src/main/java/beijinnews/example/ldgd/beijingnews/View/NoScrollViewPager.java
+++ app/src/main/java/beijinnews/example/ldgd/beijingnews/View/NoScrollViewPager.java
@@ -14,6 +14,7 @@ public class NoScrollViewPager extends ViewPager {
 
     /**
      * 通常在代码中实例化的时候用该方法
+     *
      * @param context
      */
     public NoScrollViewPager(Context context) {
@@ -22,6 +23,7 @@ public class NoScrollViewPager extends ViewPager {
 
     /**
      * 在布局文件中使用该类的时候，实例化该类用该构造方法，这个方法不能少，少的化会崩溃。
+     *
      * @param context
      * @param attrs
      */
@@ -30,7 +32,8 @@ public class NoScrollViewPager extends ViewPager {
     }
 
     /**
-     * 重写触摸事件，消费掉
+     * 重写触摸事件，消费掉（使其不可滚动）
+     *
      * @param ev
      * @return
      */
@@ -39,11 +42,12 @@ public class NoScrollViewPager extends ViewPager {
         return true;
     }
 
-/*    @Override
+
+
+    @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         return false;
-    }*/
-
+    }
 
 
 }
